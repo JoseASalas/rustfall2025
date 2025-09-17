5f79d94e8eed8dd02c8bf7f993841f1d5d2ba336
JoseASalas <64207045+JoseASalas@users.noreply.github.com>
2025-09-17 15:31:25 +0000

    Pushing for the Class

--- assignment_1/src/main.rs
+++ assignment_1/src/main.rs
@@ -1,13 +1,13 @@
 fn fahrenheit_to_celsius(f: f64) -> f64{
     let far = f;
-    let mut cel = (far - freezing) * (5/9);
-    return (cel);
+    let mut cel = (far - 32.0) * (5/9);
+    return cel;
 }
 
 fn celsius_to_fahrenheit(c: f64) -> f64{
-    let celc = c;
-    let mut fare = (celc * (9/5)) + freezing;
-    return (fare);
+    let cel = c;
+    let mut far = (cel * (9/5)) + 32.0;
+    return far;
 }
 
 fn assignment_1(){
---
+++ assignment_1/target/release/deps/assignment_1-cccb1984d03a8d65.d
@@ -0,0 +1,5 @@
+/workspaces/rustfall2025/assignment_1/target/release/deps/assignment_1-cccb1984d03a8d65.d: src/main.rs
+
+/workspaces/rustfall2025/assignment_1/target/release/deps/assignment_1-cccb1984d03a8d65: src/main.rs
+
+src/main.rs:
