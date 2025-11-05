48710663af81da20e342fa19fa58b2dd394f7c25
JoseASalas <64207045+JoseASalas@users.noreply.github.com>
2025-11-05 16:38:02 +0000

    Class Work (11/5)

--- error_hand_prac/src/main.rs
+++ error_hand_prac/src/main.rs
@@ -6,35 +6,49 @@
 //    return a/b;
 //}
 
-use std::fs::File;
-use std::io::ErrorKind;
-use std::io::Read;
+//use std::fs::File;
+//use std::io::ErrorKind;
+//use std::io::Read;
 
 
-enum Result<T, E> {
-    Ok(T),
-    Err(E),
-}
+//enum Result<T, E> {
+//    Ok(T),
+//    Err(E),
+// }
 
-fn read_username_from_file() -> Result<String, io::Error> {
-    let f = File::open("username.txt");
-    let mut f = match f {
-        Ok(file) => file,
-        Err(e) => return Err(e),
-    };
-    let mut s = String::new();
-    match f.read_to_string(&mut s) {
-        Ok(_) => Ok(s),
-        Err(e) => Err(e),
+fn serde_serialize() {
+    use serde::{Serialize, Deserialize};
+    #[derive(Deserialize)]
+    #[derive(Serialize)]
+    struct Person {
+        name: String,
+        age: u8,
     }
+    let person = Person {
+        name: "John Doe".to_string(),
+        age: 30,
+    };
+    let serialized = serde_json::to_string(&person).unwrap();
+
+    println!("Serialized Person = {:?}", serialized);
+    let person:Person = serde_json::from_str::<Person>(serialized.as_str()).unwrap();
+
+    println!("Deserialized Person = {}, {}", person.name, person.age);
 }
 
-fn read_username_from_file_2() -> Result<String, io::Error> {
-    et mut s = String::new();
-    File::open("username.txt")?.read_to_string(&mut s)? //? are supposed to represent match statements
-    Ok(s)
+fn serde_deserialize(){
+    use serde::{Serialize, Deserialize};
+    #[derive(Deserialize)]
+    struct Person {
+        name: String,
+        age: u8,
+    }
+    let data = r#"{"name": "John Doe", "age": 30}"#;
+    let person: Person = serde_json::from_str::<Person>(data).unwrap();
+    println!("Deserialized Person = {}, {}", person.name, person.age);
 }
 
 fn main() {
-    read_username_from_file();
+    serde_serialize();
+    serde_deserialize();
 }
